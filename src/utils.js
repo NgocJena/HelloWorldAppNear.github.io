b42efe2785b2c6f21e97d257206ec1a7450addad
kquan <kquan99dk@gmail.com>
2022-04-16 18:34:55 +0700

    update

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,10 @@
 import { connect, Contract, keyStores, WalletConnection } from 'near-api-js'
 import getConfig from './config'
 
-const nearConfig = getConfig(process.env.NODE_ENV || 'development')
+// const nearConfig = getConfig(process.env.NODE_ENV || 'development')
+// fix
+const nearConfig = getConfig("development");
+
 
 // console.log(nearConfig)
 
